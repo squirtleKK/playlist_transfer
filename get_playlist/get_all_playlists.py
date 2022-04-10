3121c90c34d354cd41f6d845211edc44fd943839
squirtleKK <krasnoperov2006@gmail.com>
2022-04-10 16:28:45 +0700

    пара <a> кнопок и немного выгрузки (не рабочей)

--- get_playlist/get_all_playlists.py
+++ get_playlist/get_all_playlists.py
@@ -1,1 +1,37 @@
+from doctest import master
 import requests
+from abc import ABC, abstractmethod
+
+
+
+class Platforms ():
+    @abstractmethod
+    def get_playlists():
+        pass
+    @abstractmethod
+    def add_music_to_playlist():
+        pass
+    @abstractmethod
+    def add_music ():
+        pass
+
+
+class Vk_playlist (Platforms):
+    def __init__(self, user_id, token) -> None:
+        self.user_id = user_id
+        self.token = token
+    def get_playlists(self, user_id, token):
+        method = "https://api.vk.com/method/"
+        requests.post ()
+    def add_music_to_playlist(self, audio_id, owner_id, playlist_id):
+        method = "https://api.vk.com/method/audio.add"
+        data = {
+            "access_token": self.token, 
+            "v": "5.131",
+            "owner_id":  owner_id,   
+            "playlist_id": playlist_id,
+            "audio_id": audio_id
+        }
+        requests.post (method, data)
+
+a = Vk_playlist(8132546, token)
--- server/server.py
+++ server/server.py
@@ -8,3 +8,9 @@ socketio = SocketIO(app)
 def start_page ():
     return render_template ("index.html")
 
+# req: https://oauth.vk.com/authorize?client_id=8132546&scope=audio&display=page&response_type=token&v=5.131
+
+
+# https://oauth.vk.com/authorize?client_id=8132546&display=page&redirect_uri=http://127.0.0.1:3000&scope=audio&response_type=code&v=5.131
+if __name__ == "__main__":
+    app.run (port=3000)
